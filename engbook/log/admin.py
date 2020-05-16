85704c04930e27fb46d1cf06fe93f373a18b5129
Shreshth Arora <sarora_be18@thapar.edu>
2020-05-16 08:37:18 +0530

    admin system

--- engbook/log/admin.py
+++ engbook/log/admin.py
@@ -1,6 +1,51 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+
 from .models import Logger, LogFile, LogURL
-# Register your models here.
-admin.site.register(Logger)
-admin.site.register(LogFile)
-admin.site.register(LogURL)
+
+
+@admin.register(Logger)
+class LoggerAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'slug',
+        'date_created',
+        'published',
+        'reviewed',
+        'note',
+        'user',
+        'date_modified',
+        'project',
+    )
+    list_filter = (
+        'date_created',
+        'published',
+        'reviewed',
+        'user',
+        'date_modified',
+        'project',
+    )
+    search_fields = ('slug',)
+
+
+@admin.register(LogFile)
+class LogFileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'slug',
+        'date_created',
+        'published',
+        'reviewed',
+        'file',
+        'filetype',
+    )
+    list_filter = ('date_created', 'published', 'reviewed')
+    search_fields = ('slug',)
+
+
+@admin.register(LogURL)
+class LogURLAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'log')
+    list_filter = ('log',)
--- engbook/project/admin.py
+++ engbook/project/admin.py
@@ -1,5 +1,40 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Project, Team
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(Team)
+
+from .models import Team, Project
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'slug',
+        'date_created',
+        'published',
+        'reviewed',
+        'description',
+        'token',
+    )
+    list_filter = ('date_created', 'published', 'reviewed')
+    raw_id_fields = ('members',)
+    search_fields = ('slug',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'slug',
+        'date_created',
+        'published',
+        'reviewed',
+        'team',
+        'access_token',
+        'description',
+        'image',
+        'logo',
+    )
+    list_filter = ('date_created', 'published', 'reviewed', 'team')
+    search_fields = ('slug',)
