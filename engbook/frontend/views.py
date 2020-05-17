9a2a74d97e8b43389940271e7da8e6769d373740
Shreshth Arora <sarora_be18@thapar.edu>
2020-05-17 06:06:57 +0530

    homepage

--- engbook/frontend/views.py
+++ engbook/frontend/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'frontend/index.html')
+
+
+def homepage(request):
+    return render(request, 'frontend/homepage.html')
