8e1dec5dab175427f28fd7aafb9847a5119434bc
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-18 19:07:10 +0530

    QOL update

--- engbook/user/views.py
+++ engbook/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, BioUpdateForm
+from .forms import *
 
 
 def register(request):
