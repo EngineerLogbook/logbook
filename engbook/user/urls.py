199c73af8af63299ec8c9d2f938a6ad751eabb6e
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-18 16:56:14 +0530

    adding fuctionality to edit profile page

--- engbook/user/urls.py
+++ engbook/user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views as user_views
 
+app_name = 'user'
+
 urlpatterns = [
     path('', user_views.homepage),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+    path('profile-edit/', user_views.profile_edit, name='profile'),
 ]
--- engbook/user/views.py
+++ engbook/user/views.py
@@ -42,6 +42,30 @@ def profile(request):
 
     return render(request, 'user/profile.html', context)
 
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'user/profile-edit.html', context)
+
 
 def homepage(request):
     return render(request, 'user/homepage.html')
