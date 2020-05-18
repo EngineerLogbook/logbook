5436c6656306f28ef25d7b027f271f1b8d912e12
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-18 23:14:17 +0530

    removed post request for profile

--- engbook/user/views.py
+++ engbook/user/views.py
@@ -12,7 +12,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(
                 request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
+            return redirect('user/profile-edit')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
@@ -20,27 +20,6 @@ def register(request):
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
-        
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile')
-
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-
     return render(request, 'user/profile.html', context)
 
 @login_required
