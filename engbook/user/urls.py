f428e6d411cb25285d98240eafe2edd2e2e2ac0a
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-18 19:07:55 +0530

    nc

--- engbook/user/urls.py
+++ engbook/user/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views as user_views
 
-app_name = 'user'
-
 urlpatterns = [
     path('', user_views.homepage),
     path('register/', user_views.register, name='register'),
