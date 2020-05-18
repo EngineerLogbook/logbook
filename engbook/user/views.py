2b455de9390690a84cebb9624b4a5c089585aa1b
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-05-18 23:44:49 +0530

    moved b_form to p_form

--- engbook/user/views.py
+++ engbook/user/views.py
@@ -20,7 +20,7 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'user/profile.html', context)
+    return render(request, 'user/profile.html')
 
 @login_required
 def profile_edit(request):
@@ -29,22 +29,22 @@ def profile_edit(request):
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
                                    instance=request.user.profile)
-        b_form = BioUpdateForm(request.POST, request.FILES, instace=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid() and b_form.is_vaild():
+##        b_form = BioUpdateForm(request.POST, request.FILES, instace=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            b_form.save()
+##            b_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile-edit')
 
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-        b_form = BioUpdateForm(instance=request.user.profile)
+##        b_form = BioUpdateForm(instance=request.user.profile)
     context = {
         'u_form': u_form,
         'p_form': p_form,
-        'b_form':b_form
+##        'b_form':b_form
     }
 
     return render(request, 'user/profile-edit.html', context)
